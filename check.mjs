6582ebec5dc389ff4f4b752a547b6c2f91497ba3
nkqa <98631152+nkqa@users.noreply.github.com>
2026-09-11 13:28:53 +0800

    双后缀副本标记支持多重括号组 (1)(2)(3)，括号组可重复任意次

--- check.mjs
+++ check.mjs
@@ -103,6 +103,8 @@ const checks = `
   // 双后缀 + 副本标记：括号内容不限数字（1 / copy / 副本２），半角/全角/无空格都要识别
   ok(archiveExtOf('foo.mcpack.zip') === 'mcpack' && archiveExtOf('foo.mcpack (1).zip') === 'mcpack' && archiveExtOf('foo.mcpack(1).zip') === 'mcpack' && archiveExtOf('foo.mcaddon（２）.zip') === 'mcaddon' && archiveExtOf('FOO.MCPACK　（3）.ZIP') === 'mcpack', 'archiveExtOf 应识别半角/全角/无空格的副本标记双后缀');
   ok(archiveExtOf('foo.mcpack (copy).zip') === 'mcpack' && archiveExtOf('foo.mcpack（副本2）.zip') === 'mcpack' && archiveExtOf('foo.mcaddon(已转换).zip') === 'mcaddon', 'archiveExtOf 括号内容不限数字（copy/副本/文字）');
+  ok(archiveExtOf('foo.mcpack (1)(2)(3).zip') === 'mcpack' && archiveExtOf('foo.mcaddon（1）（2）.zip') === 'mcaddon', 'archiveExtOf 应识别多重括号组 (1)(2)(3)');
+  ok(packBaseName('foo.mcpack (1)(2)(3).zip') === 'foo', 'packBaseName 应整体去除多重括号组');
   ok(archiveExtOf('plain.zip') === 'zip' && archiveExtOf('pack.mcpack') === 'mcpack' && archiveExtOf('x.rar') === 'zip', 'archiveExtOf 普通 zip/单后缀/回落判定应正确');
   ok(typeof packBaseName === 'function' && packBaseName('foo.mcpack (1).zip') === 'foo' && packBaseName('bar.mcaddon（2）.zip') === 'bar' && packBaseName('foo.mcpack (copy).zip') === 'foo' && packBaseName('plain.zip') === 'plain' && packBaseName('pack.mcpack') === 'pack', 'packBaseName 应去除双后缀与副本标记（含非数字内容）得到基础名');
   ok(doLoadArchiveBatch.toString().includes('archiveExtOf(file.name)'), '批量整包模式后缀识别应复用 archiveExtOf');
